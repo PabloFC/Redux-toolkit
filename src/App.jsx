95e0e9489bd7deef73f45f462868549239f58b12
PabloFC <pablofc_68@hotmail.com>
2024-07-02 13:08:48 +0200

    adding buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { increment } from "./store/slices/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./store/slices/counter/counterSlice";
 
 function App() {
   const { counter } = useSelector((state) => state.counter);
@@ -11,17 +14,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <p>count is {counter}</p>
       <div className="card">
-        <button onClick={() => dispatch(increment())}>
-          count is {counter}
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>
+          Increment by 2
         </button>
       </div>
     </>
