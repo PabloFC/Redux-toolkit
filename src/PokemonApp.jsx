f190b60325f804d91c4aa2326974db85af7917a9
PabloFC <pablofc_68@hotmail.com>
2024-07-02 14:17:44 +0200

    pokemonSlice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,15 @@
+const PokemonApp = () => {
+  return (
+    <div>
+      <h1>Pokemon app</h1>
+      <hr />
+      <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+        <li>Hola</li>
+      </ul>
+    </div>
+  );
+};
+
+export default PokemonApp;
